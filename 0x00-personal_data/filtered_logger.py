8a00d7734bffa9a983dcd0afd581a1f9683924e2
konditi1 <konditi1@gmail.com>
2024-02-10 15:21:34 +0300

    task 0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Encrypt password
+"""
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """Filters values from log records"""
+    for field in fields:
+        message = re.sub(f"{field}=.+?{separator}", f"{field}={redaction}{separator}", message)
+    return message
