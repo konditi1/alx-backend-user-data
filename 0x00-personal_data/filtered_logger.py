d8c9ab80ac97705ef815558bc5cae15ded86e081
konditi1 <konditi1@gmail.com>
2024-02-10 16:29:59 +0300

    task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,8 +6,10 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """Filters values from log records"""
     for field in fields:
-        message = re.sub(f"{field}=.+?{separator}", f"{field}={redaction}{separator}", message)
+        message = re.sub(f"{field}=.+?{separator}",
+                         f"{field}={redaction}{separator}", message)
     return message
