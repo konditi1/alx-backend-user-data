12b9f3c49282b4adbbc6606c6ed4d7ac04d251a8
konditi1 <konditi1@gmail.com>
2024-02-10 20:08:14 +0300

    using bcrypt.hashpaw to encrypt password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
+"""
+encrypt_password
+"""
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """Hash password"""
+    """
+    Hash password
+    :param password:
+    :return:
+    """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
