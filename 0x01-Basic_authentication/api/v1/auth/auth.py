6592e583c48d09e21d1c1bdb414ff11866491c19
konditi1 <konditi1@gmail.com>
2024-02-13 23:25:41 +0300

    implemented class Auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,10 +15,10 @@ class Auth():
         """
         if excluded_paths is None or excluded_paths == []:
             return True
-        if path in excluded_paths:
-            return False
         if path[-1] != '/':
             path += '/'
+        if path in excluded_paths:
+            return False
         return True
 
     def authorization_header(self, request=None) -> str:
