9978b2e3d52f36a655036d34726fb24c42b1190f
konditi1 <konditi1@gmail.com>
2024-02-13 12:55:06 +0300

    added error handling for unauthorised 401

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -18,7 +18,7 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 def not_found(error) -> str:
     """ Not found handler
     """
-    return jsonify({"error": "Not found"}), 404
+    return jsonify({"error": "Unauthorized"}), 404
 
 
 @app.errorhandler(401)
