15b6eabe18f3eff782f9e1f709ccd64df4e633d6
konditi1 <konditi1@gmail.com>
2024-02-17 23:00:35 +0300

    documentation

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """
-Define a module that implements Session
-Authentication.
+Define a module that implements Session Authentication.
 """
 from flask import request
 from .auth import Auth
@@ -13,107 +12,89 @@ from models.user import User
 class SessionAuth(Auth):
     """
     An instance of Session Authentication.
+
+    Attributes:
+        user_id_by_session_id (dict): A dictionary mapping session IDs to user IDs.
+
+    Methods:
+        create_session(user_id: str = None) -> str: Creates a session for the given user.
+        user_id_for_session_id(session_id: str = None) -> str: Retrieves the user ID
+        current_user(request=None): Obtains an instance for the current user.
+        destroy_session(request=None) -> bool: Invalidates the current session for the given request.
     """
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
         """
-        Create a session for the given
-        user.
+        Create a session for the given user.
 
         Parameters:
-            user_id : str, optional
-            The id for a user who needs
-            a new session. Default value
-            ensures method fails its
-            operation.
+            user_id (str): The ID of the user for whom a new session is created.
 
-        Return:
-            A uuid4 string representing the
-            session created. None is
-            returned should the function
-            fail its operation.
+        Returns:
+            str: A uuid4 string representing the created session. 
+                 None is returned if the operation fails.
         """
-        if (not user_id or not isinstance(user_id, str)):
-            return (None)
+        if not user_id or not isinstance(user_id, str):
+            return None
 
         session_id = str(uuid4())
         self.user_id_by_session_id[session_id] = user_id
 
-        return (session_id)
+        return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """
-        Retrieve the id for the user who
-        owns the session with the given
-        id.
+        Retrieve the user ID for the session with the given ID.
 
         Parameters:
-            session_id  : str, optional
-            The id for the session whose
-            user has to be retrieved. Default
-            value ensures method fails its
-            operation.
+            session_id (str): The ID of the session.
 
-        Return:
-            The id for the user with the
-            given session. None is returned
-            should the function fail its
-            operation.
+        Returns:
+            str: The user ID associated with the session ID.
+                 None is returned if the session ID is invalid or not found.
         """
-        if (not session_id or not isinstance(session_id, str)):
-            return (None)
+        if not session_id or not isinstance(session_id, str):
+            return None
 
-        return (self.user_id_by_session_id.get(session_id))
+        return self.user_id_by_session_id.get(session_id)
 
     def current_user(self, request=None):
         """
-        Obtain an instance for the current
-        user.
+        Obtain an instance for the current user.
 
         Parameters:
-            request : LocalProxy
-            A Flask request object to procss.
+            request: The Flask request object.
 
         Returns:
-            An instance for the active user
-            otherwise None.
+            User: An instance representing the current user.
+                  None is returned if the session is not found or invalid.
         """
         session_id = self.session_cookie(request)
         user_id = self.user_id_for_session_id(session_id)
 
-        return (User.get(user_id))
+        return User.get(user_id)
 
     def destroy_session(self, request=None) -> bool:
         """
-        Invalide the current session for the
-        given request.
+        Invalidate the current session for the given request.
 
-        Paameters:
-            request : LocalProxy
-            A Flask request object to process.
+        Parameters:
+            request: The Flask request object.
 
-        Return:
-            A boolean indicating whether the
-            session is destroyed or nt.
+        Returns:
+            bool: True if the session is successfully destroyed, False otherwise.
         """
-        if (not request):
-            return (False)
+        if not request:
+            return False
 
-        # Return false flag when the request
-        # has no cookie (or the value None).
         session_id = self.session_cookie(request)
-        if (not session_id):
-            return (False)
+        if not session_id:
+            return False
 
-        # Return false flag when session for
-        # some reason is not for the current
-        # user.
-        if (not self.user_id_for_session_id(session_id)):
-            return (False)
+        if not self.user_id_for_session_id(session_id):
+            return False
 
-        # Delete user's session.
         del self.user_id_by_session_id[session_id]
 
-        return (True)
-    
+        return True
