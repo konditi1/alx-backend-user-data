e15cf4482532ccbe4fd6afb470e9febf7d6bdd17
konditi1 <konditi1@gmail.com>
2024-02-21 18:52:16 +0300

    implemented register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,39 +1,44 @@
 #!/usr/bin/env python3
 """
-hash_password
+Hash password
 """
 import bcrypt
-from sqlalchemy.exc import NoResultFound
-
-from db import DB
-from user import User
+# from db import DB
+# from sqlalchemy.orm.exc import NoResultFound
+# from user import User
 
 
 def _hash_password(password: str) -> bytes:
-    """Hash password
-    Args:
-        password (str): password
-    """
-    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    """ methods that takes in a string arg and returns bytes
+    Hashes the input password using bcrypt."""
+    # Generate a salt and hash the password
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
 
 
-class Auth:
-    """Auth class to interact with the authentication database.
-    """
-
-    def __init__(self):
-        self._db = DB()
-
-    def register_user(self, email: str, password: str) -> User:
-        """Register user
-        Args:
-            email (str): email
-            password (str): password
-        """
-        try:
-            user = self._db.find_user_by(email=email)
-        except NoResultFound:
-            hashed_password = _hash_password(password)
-            user = self._db.add_user(email, hashed_password)
-            return user
-        raise ValueError("User {} already exists".format(email))
+# class Auth:
+#     """Auth class to interact with the authentication database.
+#     """
+#
+#     def __init__(self):
+#         """init function"""
+#         self._db = DB()
+#
+#     def register_user(self, email: str, password: str) -> User:
+#         """
+#         Register a new user
+#         """
+#         try:
+#             user = self._db.find_user_by(email=email)
+#         except NoResultFound:
+#             user = None
+#         if user is None:
+#             try:
+#                 hashed_password = _hash_password(password)
+#                 new_user = self._db.add_user(email, hashed_password)
+#                 return new_user
+#             except ValueError:
+#                 raise ValueError("User <user's email> already exists")
+#         else:
+#             raise ValueError(f"User {email} already exists")
